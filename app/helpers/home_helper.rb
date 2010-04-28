edcf14a5d1a9c0f427f639c22f14d3db9126926c
Srinivas Aki <srinivas.aki@gmail.com>
2010-04-27 23:46:33 -0700

    Adding interactive google geocode map Linking respective states to their pages

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -13,4 +13,59 @@ module HomeHelper
     %Q{<img src="http://chart.apis.google.com/chart?cht=t&chs=440x220&chd=s:#{chd}&chco=#{DEFAULT_STATE_COLOR},#{SUPPORTED_STATE_COLOR},#{PENDING_STATE_COLOR}&chld=#{chld}&chtm=usa&chf=bg,s,#{MAP_BG_COLOR}">}.html_safe!
   end
 
+  def interactive_us_map_image_tag
+    states = State.all
+    states = states.collect {|s| [s.region_code, s.supported? , s.name]}
+
+    data = states.to_json
+
+    x = <<-STR
+      <div id='map_canvas'></div>
+      <script type='text/javascript' src='http://www.google.com/jsapi'></script>
+        <script type='text/javascript'>
+         google.load('visualization', '1', {'packages': ['geomap']});
+         google.setOnLoadCallback(drawMap);
+
+          function drawMap() {
+            var data = new google.visualization.DataTable();
+            data.addRows(2);
+            data.addColumn('string', 'State');
+            data.addColumn('number', 'Abbreviation');
+            data.addColumn('string', 'Hover');
+
+            data.setValue(0, 0, 'US-CA');
+            data.setValue(0, 1, 300);
+            data.setValue(0, 2, 'California');
+
+            data.setValue(1, 0, 'US-FL');
+            data.setValue(0, 1, 500);
+            data.setValue(1, 2, 'Florida');
+
+            var options = {};
+            options['dataMode'] = 'regions';
+            options['region'] = 'US';
+            options['width'] = jQuery(document).width()/1.3;
+            options['height'] = jQuery(document).height()/1.8;
+
+            var container = document.getElementById('map_canvas');
+            var geomap = new google.visualization.GeoMap(container);
+
+            geomap.draw(data, options);
+
+            google.visualization.events.addListener(geomap, 'regionClick', myHandler);
+          };
+
+          function myHandler(e){
+            console.debug('event raised');
+            jQuery(window.location.href = 'states/' + e['region']);
+            console.debug('where is the selection');
+            console.debug(selection);
+          };
+
+
+      </script>
+    STR
+
+    x.html_safe!
+  end
 end
