423ada42c852be21086f60b21afc35ed8dc5f413
Carl Tashian <carl@ppolitics.org>
2010-05-11 13:18:18 -0700

    List people under states/{state}/reps rather than the top-level state page

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -8,11 +8,6 @@ class BillsController < ApplicationController
       @bill = state && state.bills.find_by_param(params[:id])
     end
 
-    if params[:person_id] && params[:id]
-      person = Person.find(params[:person_id])
-      @bill = person && Bill.find_by_param(params[:id]) 
-    end
-
     @bill || resource_not_found
   end
 end
