df3b13347e26057b2755802f825d4f85e7062d52
Carl Tashian <carl@ppolitics.org>
2010-05-12 17:15:45 -0700

    Add bill sponsorship statistics

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,3 +1,12 @@
 module PeopleHelper
-
+  def sponsorship_vitals_for(person)
+     t = "<span class='sponsorship_vitals'>"
+     if s = @person.current_sponsorship_vitals
+      t += link_to(s.bill_count, sponsored_bills_person_url(@person)) + " bills sponsored or co-sponsored <span class='rank'>(ranks " + s.rank.to_i.ordinalize + " out of " + s.total_sponsors + ")</span>"
+    else
+      t += "No sponsorship information available"
+    end
+    t += "</span>"
+    t.html_safe!
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -35,7 +35,38 @@ class Person < ActiveRecord::Base
   def youtube_url
     youtube_id.blank? ? nil : "http://www.youtube.com/user/" + youtube_id
   end
-
+  
+  def current_sponsorship_vitals
+    Person.find_by_sql(["
+      select * from (
+        select
+        -- this is a subquery because we want the max() to look at all rows
+        y.sponsor_id,
+        y.bill_count,
+        y.rank,
+        -- we have to do a max() here because you can't do max(row_number() over ..)
+        max(y.rnum) over (partition by y.chamber_id) as total_sponsors
+        from (
+          select s.sponsor_id, s.chamber_id, s.bill_count, s.session_id,
+          -- rank may have duplicates (3 people with rank #3),
+          -- row_number does not.
+          row_number() over w as rnum,
+          rank() over w as rank
+          from
+            (select r.chamber_id, r.session_id, s0.sponsor_id,
+            -- we're doing this subquery for the bill_count.
+            count(*) as bill_count
+            from sponsorships s0
+            inner join roles r on s0.sponsor_id = r.person_id
+            where (current_date between r.start_date and r.end_date)
+            group by r.chamber_id, s0.sponsor_id, r.session_id) s
+          window w as (partition by s.chamber_id, s.session_id order by s.bill_count desc)
+        ) y
+      ) z
+      where z.sponsor_id = ?
+      limit 1", id]).first
+  end
+  
   def to_param
    "#{id}-#{full_name.parameterize}"
   end
