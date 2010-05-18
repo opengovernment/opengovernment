ce25c1df092260dd479386dd07dc62b61d43ad30
Carl Tashian <carl@ppolitics.org>
2010-05-18 07:26:53 -0700

    Add constraints to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+execute "ALTER TABLE districts
+ADD CONSTRAINT districts_state_fk
+FOREIGN KEY (state_id) REFERENCES states (id);"
+
+execute "ALTER TABLE legislatures
+ ADD CONSTRAINT legislatures_state_fk
+ FOREIGN KEY (state_id) REFERENCES states (id);"
+
+execute "ALTER TABLE chambers
+  ADD CONSTRAINT chamber_legislature_fk
+  FOREIGN KEY (legislature_id) REFERENCES legislatures (id);"
+
+execute "ALTER TABLE sessions
+ ADD CONSTRAINT session_legislature_fk
+ FOREIGN KEY (legislature_id) REFERENCES legislatures (id);"
+
+execute "ALTER TABLE roles
+ ADD CONSTRAINT role_person_fk
+ FOREIGN KEY (person_id) REFERENCES people (id);"
+
+execute "ALTER TABLE roles
+ ADD CONSTRAINT role_state_fk
+ FOREIGN KEY (state_id) REFERENCES states (id);"
+
+execute "ALTER TABLE roles
+ ADD CONSTRAINT role_district_fk
+ FOREIGN KEY (district_id) REFERENCES districts (id);"
+
+execute "ALTER TABLE roles
+ ADD CONSTRAINT role_chamber_fk
+ FOREIGN KEY (chamber_id) REFERENCES chambers (id);"
+
+execute "ALTER TABLE roles
+ ADD CONSTRAINT role_session_fk
+ FOREIGN KEY (session_id) REFERENCES sessions (id);"
+
+execute "ALTER TABLE addresses
+ ADD CONSTRAINT address_person_fk
+ FOREIGN KEY (person_id) REFERENCES people (id);"
