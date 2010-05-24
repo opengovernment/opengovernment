eebd9968e3d5f12a7541b76bd82f42d28d0e364a
Carl Tashian <carl@ppolitics.org>
2010-05-24 15:59:50 -0700

    List people on state page

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,6 +2,9 @@ class StatesController < ApplicationController
   before_filter :get_state
 
   def show
+    @state_lower_chamber_roles = Role.current_chamber_roles(@state.legislature.lower_chamber)
+    @state_upper_chamber_roles = Role.current_chamber_roles(@state.legislature.upper_chamber)
+    @federal_lower_chamber_roles = Role.current.for_chamber(Legislature::CONGRESS.lower_chamber).for_state(@state).scoped({:include => [:district, :chamber, :person]})
     @state_key_votes = Bill.all(:conditions => {:votesmart_key_vote => true, :chamber_id => @state.legislature.chambers})
   end
 
@@ -16,12 +19,10 @@ class StatesController < ApplicationController
   end
 
   protected
+
   def get_state
     #TODO: May be we should clean the routes to pass in :state as params
     @state = State.find_by_slug(params[:id], :include => {:legislature => {:upper_chamber => :districts, :lower_chamber => :districts}})
-    @state_lower_chamber_roles = Role.current_chamber_roles(@state.legislature.lower_chamber)
-    @state_upper_chamber_roles = Role.current_chamber_roles(@state.legislature.upper_chamber)
-    @federal_lower_chamber_roles = Role.current.for_chamber(Legislature::CONGRESS.lower_chamber).for_state(@state).scoped({:include => [:district, :chamber, :person]})
 
     @state || resource_not_found
   end
