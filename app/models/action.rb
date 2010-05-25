806a23c820fc4a5090050cfc972d946f7fb1095d
Carl Tashian <carl@ppolitics.org>
2010-05-25 11:23:17 -0700

    New date format :pretty

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,12 +2,10 @@ class Action < ActiveRecord::Base
   belongs_to :bill
 
   def description
-    "The " + actor.capitalize + " chamber performed '" + action + "' on " + pretty_date
+    "The " + actor.capitalize + " chamber performed '" + action + "' on " + date.to_s(:pretty)
   end
   
-  def pretty_date
-    s = self[:date].strftime("%B %d")
-    s << self[:date].strftime(", %Y") if self[:date].year != Time.now.year
-    s
+  def actor_fm
+    actor.capitalize
   end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,7 +1,14 @@
-ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :default => '%B %e, %Y',
   :slashed => '%m/%d/%Y',
   :no_year => '%B %e, %a',
   :date_time12  => "%m/%d/%Y %I:%M%p",
-  :date_time24  => "%m/%d/%Y %H:%M"
+  :date_time24  => "%m/%d/%Y %H:%M",
+  :pretty => lambda do |d|
+      if d.year != Time.now.year
+        d.strftime("%B %e")
+      else
+        d.strftime("%B %e, %Y") 
+      end
+    end
 )
