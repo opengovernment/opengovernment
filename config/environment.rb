7c8564e34780e24a7c7ea0c9271a5cbc7ce486cc
Carl Tashian <carl@ppolitics.org>
2010-05-25 10:52:22 -0700

    Change committee => committees and chamber => chambers to avoid namespace conflict

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,7 @@ Rails::Initializer.run do |config|
   # -- all .rb files in that directory are automatically loaded.
 
   # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/app/models/committee #{RAILS_ROOT}/app/models/chamber )
+  config.load_paths += %W( #{RAILS_ROOT}/app/models/committees #{RAILS_ROOT}/app/models/chambers )
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
