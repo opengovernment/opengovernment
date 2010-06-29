90b87c8d653bb745eb75690b55567ed948ed470c
Srinivas Aki <srinivas.aki@gmail.com>
2010-06-28 17:24:37 -0700

    Fixing bill path

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 OpenGov::Application.routes.draw do |map|
   match '/search' => 'districts#search', :as => 'search'
   match '/states/ca/subscriptions' =>  'states#subscribe', :as => 'state_subscriptions'
+  match '/states/:state_id/sessions/:session_id/bills/:id' =>  'bills#show', :as => 'bill'
 
   namespace :admin do
     resources :states
   end
 
   resources :states do
-    resources :bills do
-      member do
-        get :show, :path => '/states/:state_id/sessions/:session'
-      end
-    end
-
     resources :votes do
       member do
         get :show
@@ -27,7 +22,7 @@ OpenGov::Application.routes.draw do |map|
         get :upper
         get :lower
         get :joint
-      end      
+      end
     end
 
     resources :bills do
