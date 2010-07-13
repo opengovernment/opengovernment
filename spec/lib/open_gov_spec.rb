dde563ce9785d56ff16c46f46f05b5791ed45825
Carl Tashian <carl@ppolitics.org>
2010-07-13 14:48:27 -0700

    Spec compatibility fixes for new rspec

--- spec/lib/open_gov_spec.rb
+++ spec/lib/open_gov_spec.rb
@@ -4,19 +4,19 @@ require 'lib/open_gov'
 describe OpenGov::Resources do
   context ".valid_date!" do
     it "should return a valid date given a float" do
-      OpenGov::Resources::valid_date!(1242187200.0).should eql(Date.parse("Tue, 12 May 2009"))
+      OpenGov::Resources::valid_date!(1242187200.0).should == Date.parse("Tue, 12 May 2009")
     end
 
     it "should return a date given a string" do
-      OpenGov::Resources::valid_date!("Tue, 12 May 2009").should eql(Date.parse("Tue, 12 May 2009"))
+      OpenGov::Resources::valid_date!("Tue, 12 May 2009").should == Date.parse("Tue, 12 May 2009")
     end
     
     it "should return a date given a date" do
-      OpenGov::Resources::valid_date!(Date.parse("Tue, 12 May 2009")).should eql(Date.parse("Tue, 12 May 2009"))
+      OpenGov::Resources::valid_date!(Date.parse("Tue, 12 May 2009")).should == Date.parse("Tue, 12 May 2009")
     end
 
     it "should return nil if passed nil" do
-      OpenGov::Resources::valid_date!(nil).should be_nil
+      OpenGov::Resources::valid_date!(nil).should == nil
     end
 
     it "should raise a TypeError for an unrecognized type" do
