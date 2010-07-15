4853b4d2aed050811d94328527a878f193388867
Carl Tashian <carl@ppolitics.org>
2010-07-15 10:43:11 -0700

    Map legend from GeoServer

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -101,4 +101,17 @@ module VotesHelper
   def vote_map_img(state, chamber_id, vote_id)
     return image_tag(eval('"' + MAP_URL + '"'), :width => MAP_WIDTH, :height => MAP_HEIGHT, :class => 'vote_map', :alt => "Geography of the vote")
   end
+  
+  LEGEND_URL = %q(http://localhost:8080/geoserver/wms?
+    REQUEST=GetLegendGraphic
+    &VERSION=1.0.0
+    &FORMAT=image/png
+    &WIDTH=20
+    &HEIGHT=20
+    &LAYER=cite:v_district_votes
+    &SLD=#{u('http://localhost:3000/votes.xml')}).gsub(/\n\s+/,'')
+
+  def vote_legend_img
+    return image_tag(eval('"' + LEGEND_URL + '"'), :class => 'vote_legend')
+  end
 end
