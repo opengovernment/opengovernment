564784ce830385f0c5fe82bacc2d65e5938a5e16
Srinivas Aki <srinivas.aki@gmail.com>
2010-07-27 16:26:22 -0700

    Fixed new tagging bug on a new tag creation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
   var hidden_field = $('input#tag');
-  $('#tags a').click(function(event) {
+  $('#tags a').live('click', function(event) {
     hidden_field.val($(this).html());
     $('#tagging_form').submit();
     event.preventDefault();
