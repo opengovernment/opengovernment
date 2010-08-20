69ec78ba2258b36d7a3e9e84994ec707c96e79da
tashian <carl@ppolitics.org>
2010-08-20 13:47:55 -0700

    Better styling of the US map

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,25 +1,28 @@
 module HomeHelper
   def interactive_us_map
+    # w, h, and f variables are subsitutions and are used during the eval() of the map url.
     unless ['development', 'test'].include?(Rails.env)
+      w = HomeController::MAP_WIDTH
+      h = HomeController::MAP_HEIGHT
+
       states_map_tag = Rails.cache.read('states_map_tag')
       if states_map_tag.nil?
         f = "text/html"
-        w = HomeController::MAP_WIDTH
-        h = HomeController::MAP_HEIGHT
-        uri = URI.parse(eval('"' + HomeController::MAP_POST_URL + '"'))
-        http = Net::HTTP.new(uri.host, uri.port)
+        map_uri = URI.parse(eval('"' + HomeController::MAP_POST_URL + '"'))
+        http = Net::HTTP.new(map_uri.host, map_uri.port)
         http.open_timeout = 5 # in seconds
         http.read_timeout = 5 # in seconds
 
         # The request.
-        req = Net::HTTP::Get.new(uri.request_uri)
+        req = Net::HTTP::Get.new(map_uri.request_uri)
         res = http.request(req)
 
         states_map_tag = res.body
         Rails.cache.write('states_map_tag', states_map_tag)
       end
 
-      %Q{<img src="/us_map.png" width="#{HomeController::MAP_WIDTH}" height="#{HomeController::MAP_HEIGHT}" alt="USA" usemap="#states" />}.html_safe + states_map_tag.html_safe
+      f = "image/png"
+      %Q{<img src="#{eval('"' + HomeController::MAP_POST_URL + '"')}" width="#{HomeController::MAP_WIDTH}" height="#{HomeController::MAP_HEIGHT}" alt="USA" usemap="#states" />}.html_safe + states_map_tag.html_safe
     end
   end
 end
