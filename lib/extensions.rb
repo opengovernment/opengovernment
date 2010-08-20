b136cc0042d12504bebb997e16813b8254e7ad8d
tashian <carl@ppolitics.org>
2010-08-20 09:30:54 -0700

    Add some right sidebar items

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,6 +1,7 @@
 require 'extensions/date'
 require 'extensions/integer'
 require 'extensions/bread'
+require 'extensions/string'
 if Rails && Rails.env == "test"
   require 'extensions/rspec/integration_example_group'
 end
