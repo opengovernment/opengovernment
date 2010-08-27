db97a0d8f8005c82fd24ce8cef8720734a7b9fd0
tashian <carl@ppolitics.org>
2010-08-27 14:40:04 -0700

    More appropriate settings for staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,15 +4,20 @@ OpenGov::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = false
+
+  # Disable Rails's static asset server
+  # Apache will already do this
+  config.serve_static_assets = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
