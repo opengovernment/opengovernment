d628f93053f6b9ff004fcb1b6049bf9cfaddf75c
tashian <carl@ppolitics.org>
2010-09-02 00:45:24 -0700

    move some helpers over to UrlHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,28 +23,6 @@ module ApplicationHelper
         |
     end
   end
-  
-  # Given a full or short URL, return only the domain
-  # and TLD. Examples:
-  #   http://www.opencongress.org => opencongress.org
-  #   http://news.bbc.co.uk/latest/news/ => news.bbc.co.uk
-  #   www.whitehouse.gov/obama => whitehouse.gov
-  def domain_for(url)
-    if dom = URI.parse(url).host
-      dom.sub(/www\./,'')
-    end
-  end
-
-  def link_to_with_domain(name, url, html_options = nil)
-    result = link_to(name, url, html_options) \
-      + " <span class=\"link_domain\">(".html_safe \
-      + domain_for(url) \
-      + ")</span>".html_safe
-  end
-
-  def state_url(subdomain)
-    url_for(:subdomain => (subdomain.is_a?(State) ? subdomain.abbrev.downcase : subdomain), :controller => "states", :action => "show")
-  end
 
   def photo_for(person, size = :full)
     ops = case size
