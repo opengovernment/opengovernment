e3c20fbfb05d184654f58a368d71476d8d248815
Srinivas Aki <srinivas.aki@gmail.com>
2010-09-07 16:03:21 -0700

    ajaxifying different bill sort options Added bills sort by most views, citations, recent actions, most in the news

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,7 +1,27 @@
 class BillsController < ApplicationController
   before_filter :get_state
   before_filter :get_bill, :except => [:index]
-  
+
+  def index
+    respond_to do |format|
+      format.js do
+        @bills = case params[:sort]
+          when "actions"
+            Bill.unscoped.for_state(@state).order("last_action_at desc").limit(10)
+          when "recent"
+            Bill.for_state(@state).limit(10)
+          when "citations"
+            Bill.search(:order => 'citations_count desc', :per_page => 10)
+          when "views"
+            Bill.find(Page.most_viewed('Bill').collect(&:og_object_id))
+        end
+      end
+      format.html do
+        @bills = Bill.unscoped.for_state(@state).order("last_action_at desc").limit(10)
+      end
+    end
+  end
+
   def show
     if params[:actions] && params[:actions] == "all"
       @actions = @bill.actions
