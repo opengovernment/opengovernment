a9aa39d7bd374bb6988e2d070b6cefaf8201a4cc
tashian <carl@ppolitics.org>
2010-09-09 15:55:38 -0700

    Add locale files based on subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,23 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   include UrlHelper
   helper_method :current_place, :current_place_name
+  before_filter :set_locale
 
   protect_from_forgery
   layout 'application'
   # Auth for staging environment
   USERNAME, PASSWORD = 'opengov', API_KEYS['og_staging']
+
+  def set_locale
+    I18n.locale = extract_locale_from_subdomain
+  end
+
+  # Get locale from top-level domain or return nil if such locale is not availabl
+  # You have to put something like:
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil 
+  end 
 
   protected
   def resource_not_found
