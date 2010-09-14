0c47c3bbcbbdea6ad2dce1b2436be19414e265e0
tashian <carl@ppolitics.org>
2010-09-13 18:14:25 -0700

    Simplify sort options for bills; don't link to bill sponsors if they don't exist

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,37 +3,42 @@ class BillsController < ApplicationController
   before_filter :get_bill, :except => [:index]
 
   def index
+    @sort = params[:sort] || 'introduced'
+
+    @sorts = {:introduced => 'Date Introduced',
+      :recent => 'Recent Actions',
+      :citations => 'Most In The News',
+      :views => 'Most Viewed',
+      :keyvotes => 'Key Votes'}
+
     @state_bills = Bill.for_state(@state)
+    @bills = case params[:sort]
+      when 'introduced'
+        @state_bills.order('first_action_at desc').limit(10)
+      when 'citations'
+        @state_bills.search(:order => 'citations_count desc', :per_page => 10)
+      when 'views'
+        @state_bills.find(Page.most_viewed('Bill').collect(&:og_object_id))
+      when 'keyvotes'
+        @state_bills.where(:votesmart_key_vote => true, :chamber_id => @state.legislature.chambers)
+      else
+        @state_bills.order('last_action_at desc').limit(10)
+    end
 
     respond_to do |format|
-      format.js do
-
-        @bills = case params[:sort]
-          when "actions"
-            @state_bills.order("last_action_at desc").limit(10)
-          when "recent"
-            @state_bills.limit(10)
-          when "citations"
-            @state_bills.search(:order => 'citations_count desc', :per_page => 10)
-          when "views"
-            @state_bills.find(Page.most_viewed('Bill').collect(&:og_object_id))
-          when "keyvotes"
-            legislature = @state.legislature
-            @state_bills.find(:all, :conditions => {:votesmart_key_vote => true, :chamber_id => legislature.chambers})
-        end
-      end
-      format.html do
-        @bills = @state_bills.order("last_action_at desc").limit(10)
-      end
+      format.js
+      format.html
     end
   end
 
   def show
-    if params[:actions] && params[:actions] == "all"
+    if params[:actions] && params[:actions] == 'all'
       @actions = @bill.actions
     else
       @actions = @bill.major_actions
     end
+    
+    @sponsors = @bill.sponsorships.includes(:sponsor).order("people.last_name, sponsorships.sponsor_name")
 
     respond_to do |format|
       format.js
