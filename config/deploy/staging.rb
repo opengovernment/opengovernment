8632264b30c2d779f9ae21060c465f8b32204d27
Srinivas Aki <srinivas.aki@gmail.com>
2010-09-14 12:54:39 -0700

    Adding environment_database variables

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,3 +5,9 @@ set :branch, "master"
 role :web, "dev.opencongress.org"
 role :app, "dev.opencongress.org"
 role :db,  "dev.opencongress.org", :primary => true
+
+set :staging_database, "og_production"
+set :staging_dbhost,   "localhost"
+
+set :environment_database, defer { staging_database }
+set :environment_dbhost, defer { staging_dbhost }
