538452b287d001cbec9ce58c5bad56ba957dbe5f
tashian <carl@ppolitics.org>
2010-09-22 16:01:41 -0700

    Use integers for og object ids in page views

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,7 +1,7 @@
 class View
   include MongoMapper::Document
   key :created_at
-  key :user_id
+  key :user_id, Integer
   belongs_to :page
   scope :since, lambda {|date| where(:created_at.gte => date)}
 end
