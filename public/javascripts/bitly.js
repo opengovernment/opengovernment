8d0a5f2130717ad312ff261533a13cbb80b3e10c
tashian <carl@ppolitics.org>
2010-09-23 13:13:27 -0700

    Shorten twitter links with bit.ly

---
+++ public/javascripts/bitly.js
@@ -0,0 +1,9 @@
+function BitlyAPI(login, apiKey) {
+  this.login = login;
+  this.apiKey = apiKey;
+}
+
+BitlyAPI.prototype.shorten = function(link, callback) {
+  requestURL = "http://api.bit.ly/v3/shorten?login=" + this.login + "&apiKey=" + this.apiKey + "&longUrl=" + link + "&format=json&callback=?"
+  $.getJSON(requestURL, callback);
+}
