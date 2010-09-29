cfc91d5b4749408051a38fac6d4f87c143895473
tashian <carl@ppolitics.org>
2010-09-28 18:04:13 -0700

    Get people and bills 'most viewed' list working properly

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -4,6 +4,7 @@ require 'extensions/bread'
 require 'extensions/string'
 require 'extensions/mongo_mapper'
 require 'extensions/time'
+require 'extensions/explicit_order'
 if Rails && Rails.env == "test"
   require 'extensions/rspec/integration_example_group'
 end
