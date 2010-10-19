96d493d8c55c5ee9241dca3d5ec88a47d976928d
tashian <carl@ppolitics.org>
2010-10-19 10:52:27 -0700

    Simplification of a bunch of partials; add bill list to sponsored bills

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,13 +1,6 @@
 module PeopleHelper
   def sponsorship_vitals_for(person)
-     t = "<span class='sponsorship_vitals'>".html_safe
-     if s = @person.current_sponsorship_vitals
-      t += link_to(s.bill_count, sponsored_bills_person_url(@person)) + " bills sponsored or co-sponsored <span class='rank'>(ranks ".html_safe + s.rank.to_i.ordinalize + " out of " + s.total_sponsors + ")</span>".html_safe
-    else
-      t += "No sponsorship information available"
-    end
-    t += "</span>".html_safe
-    t.html_safe
+    render 'people/sponsorship_vitals', :vitals => @person.current_sponsorship_vitals
   end
 
   def year_span(start_date, end_date)
