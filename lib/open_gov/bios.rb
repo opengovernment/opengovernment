3657ba5750543b91b48ebb4fd9bde7b406888de4
tashian <carl@ppolitics.org>
2010-11-02 16:49:25 -0400

    Fetch wikipedia bios for all candidates, not just those with votesmart ids

--- lib/open_gov/bios.rb
+++ lib/open_gov/bios.rb
@@ -4,7 +4,7 @@ module OpenGov
       puts 'Fetching bios for all current people with votesmart_ids'
 
       f, u, s = 0, 0, 0
-      Person.with_votesmart_id.with_current_role.each do |person|
+      Person.with_current_role.each do |person|
         begin
           # puts "Fetching bio data for #{person.wiki_name}"
           bio =  GovKit::SearchEngines::Wikipedia.search(person.wiki_name)
