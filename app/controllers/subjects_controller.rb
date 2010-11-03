05c88a94348eaff06b2a57dd146ed9b8117f6755
tashian <carl@ppolitics.org>
2010-11-03 17:21:05 -0400

    Added bill subjects

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,20 @@
+class SubjectsController < ApplicationController
+  before_filter :get_state
+  before_filter :get_subject, :only => :show
+
+  def index
+    @min_bills = 10
+
+    if params[:all]
+      @subjects = Subject.for_state(@state).paginate(:page => params[:page])
+    else
+      @subjects = Subject.for_state(@state).with_bill_count.having(["count(bills_subjects.id) > ?", @min_bills]).paginate(:page => params[:page])
+    end
+  end
+
+  def get_subject
+    @subject = Subject.find(params[:id])
+    @subject_bills = @subject.bills.paginate(:page => params[:page])
+    @subject || resource_not_found
+  end  
+end
