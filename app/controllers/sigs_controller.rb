4d348d877ca18d7a6c54fc2f0b010ac7ee6c13dc
tashian <carl@ppolitics.org>
2010-11-22 11:30:28 -0800

    Segregate ratings page by sig category

--- app/controllers/sigs_controller.rb
+++ app/controllers/sigs_controller.rb
@@ -8,7 +8,12 @@ class SigsController < ApplicationController
   def show
     @years_available = Rating.select("ratings.timespan").where(:sig_id => @sig.id).group(:timespan).collect { |r| r.timespan.to_s }.sort {|x,y| y <=> x }
     @year = params[:year] || @years_available.first
-    @ratings = @sig.ratings.where(:timespan => @year)
+
+
+    @categories_available = Rating.select("ratings.rating_name").where(:sig_id => @sig.id, :timespan => @year).group(:rating_name).collect { |r| r.rating_name }.sort {|x,y| y <=> x }
+    @category = params[:category] || @categories_available.first
+
+    @ratings = @sig.ratings.where(:timespan => @year, :rating_name => @category)
   end
 
   protected
