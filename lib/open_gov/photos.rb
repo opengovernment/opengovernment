49cd7ae64092d8fbff1f9c719b4206c90a13f4aa
tashian <carl@ppolitics.org>
2011-01-18 15:39:00 -0800

    Keep count of votesmart photos updated

--- lib/open_gov/photos.rb
+++ lib/open_gov/photos.rb
@@ -2,7 +2,7 @@ module OpenGov
   class Photos < Resources
     def self.import!
       puts "Importing photos from VoteSmart"
-      i = 0
+      i, s = 0, 0
       Person.with_votesmart_id.with_current_role.where("photo_url is null").each do |person|
         begin
           bio = GovKit::VoteSmart::Bio.find(person.votesmart_id)
@@ -14,12 +14,13 @@ module OpenGov
             #puts "Updating #{person.to_param}"
             person.save
           else
-            puts "Skipping..no photo found"
+            s += 1
           end
         rescue GovKit::ResourceNotFound
           puts "No bio found for #{person.to_param}"
         end	 	
       end 	
+      puts "#{i} photos found; #{s} skipped."
     end
   end
 end
