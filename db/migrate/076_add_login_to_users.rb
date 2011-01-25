e4b68255a6ac1967657c05ae0824ce19956da49b
tashian <carl@ppolitics.org>
2011-01-25 12:49:19 -0800

    Actually, we do want to migrate the users table on production. The migration won't use the OpenCongress connection

--- db/migrate/076_add_login_to_users.rb
+++ db/migrate/076_add_login_to_users.rb
@@ -1,15 +1,10 @@
 class AddLoginToUsers < ActiveRecord::Migration
   def self.up
-    # only migrate systems that don't tie into the opencongress database
-    unless ActiveRecord::Base.configurations.has_key?('opencongress')
-      add_column :users, :login, :string
-      add_column :users, :salt, :string, :limit => 40
-    end
+    add_column :users, :login, :string
+    add_column :users, :salt, :string, :limit => 40
   end
 
   def self.down
-    unless ActiveRecord::Base.configurations.has_key?('opencongress')
-      remove_column :users, :login, :salt
-    end
+    remove_column :users, :login, :salt
   end
 end
