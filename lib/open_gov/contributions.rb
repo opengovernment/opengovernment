f1da4e9248c184e94fd0ea4d0e92aa87cf94f8de
tashian <carl@ppolitics.org>
2011-02-02 10:26:09 -0800

    Load contributions faster and delete previous ones properly

--- lib/open_gov/contributions.rb
+++ lib/open_gov/contributions.rb
@@ -1,5 +1,16 @@
 module OpenGov
   class Contributions < Resources
+
+    @@states = {}
+
+    def self.build_state_hash
+      # Cache all of the ids of people so we don't have to keep looking them up.
+      if @@states.size == 0
+        State.all.each do |s|
+          @@states[s.abbrev.upcase] = s.id
+        end
+      end
+    end
     
     # This value for transaction_namespace= in TD API cals limits our queries with TransparencyData to the NIMSP dataset.
     IMSP_NAMESPACE = 'urn:nimsp:recipient'
@@ -11,44 +22,42 @@ module OpenGov
     end
 
     def self.import_state(state, options = {})
-      puts "Deleting existing contributions in #{state.abbrev}."
-
-      Contribution.delete_all(['person_id in (select person_id from roles where state_id = ? or (district_id in (select id from districts where state_id = ?)))', state.id, state.id])
-
+      build_state_hash
+      
       Person.find_by_sql(['SELECT distinct people.* FROM "roles" INNER JOIN "people" ON "people"."id" = "roles"."person_id" WHERE (roles.district_id in (select id from districts where state_id = ?) or roles.state_id = ?) AND (people.transparencydata_id is not null)', state.id, state.id]).each do |person|
+        puts "Deleting contributions for #{person.full_name}"
+        Contribution.delete_all(:person_id => person.id)        
+        
         puts "Importing contributions for #{person.full_name}"
         total = 0
 
-        begin
-          entity = GovKit::TransparencyData::Entity.find_by_id(person.transparencydata_id)
-          entity.external_ids.each do |eid|
-            page = 0
-            contributions = []
+        entity = GovKit::TransparencyData::Entity.find_by_id(person.transparencydata_id)
+        entity.external_ids.each do |eid|
+          page = 0
+          contributions = []
 
-            # Fetch the NIMSP external ids only.
-            # puts "fetching '#{eid[:namespace]}' '#{eid[:id]}'"
-            if eid[:namespace].eql?(IMSP_NAMESPACE)
-              # Loop to get all contributions
+          # Fetch the NIMSP external ids only.
+          # puts "fetching '#{eid[:namespace]}' '#{eid[:id]}'"
+          if eid[:namespace].eql?(IMSP_NAMESPACE)
+            # Loop to get all contributions
+            begin
+              page += 1
               begin
-                page += 1
-                begin
-                  contributions = GovKit::TransparencyData::Contribution.find(:recipient_ext_id => eid[:id], :recipient_type => 'P', :page => page)
-
-                  contributions.each do |contribution|
-                    make_contribution(person, contribution)
-                  end
-                  # process them.
-                rescue Crack::ParseError => e
-                  puts e.class.to_s + ": Invalid JSON for person " + person.transparencydata_id
-                  break
-                rescue GovKit::ResourceNotFound => e
-                  puts "Got resource not found."
-                  break
+                contributions = GovKit::TransparencyData::Contribution.find(:recipient_ext_id => eid[:id], :recipient_type => 'P', :page => page)
+                
+                contributions.each do |contribution|
+                  make_contribution(person, contribution)
                 end
-                total += contributions.size
-              end while contributions.size > 0
-            end
-
+                # process them.
+              rescue Crack::ParseError => e
+                puts e.class.to_s + ": Invalid JSON for person " + person.transparencydata_id
+                break
+              rescue GovKit::ResourceNotFound => e
+                puts "Got resource not found."
+                break
+              end
+              total += contributions.size
+            end while contributions.size >= 1000
           end
         end
 
@@ -56,13 +65,15 @@ module OpenGov
       end
     end
 
+    private
+
     def self.make_contribution(person, con)
       begin
         contribution = Contribution.create(
           :person_id => person.id,
           :state_id => person.state_id,
           :industry_id => con.contributor_category,
-          :contributor_state_id => con.contributor_state.blank? ? nil : State.find_by_abbrev(con.contributor_state).try(:id),
+          :contributor_state_id => @@states[con.contributor_state.upcase],
           :contributor_occupation => con.contributor_occupation,
           :contributor_employer => con.contributor_employer,
           :amount => con.amount,
