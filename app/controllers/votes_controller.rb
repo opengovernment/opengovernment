0bab207b3e51de85605904b3df4890e8ae308f37
tashian <carl@ppolitics.org>
2011-03-03 15:32:12 -0800

    Add support for importing openstates vote ids and linking with them when available

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,12 @@ class VotesController < SubdomainController
   before_filter :get_vote
 
   def get_vote
-    @vote = Vote.find(params[:id], :include => [:bill])
+    begin
+      @vote = Vote.find(params[:id], :include => [:bill])
+    rescue ActiveRecord::RecordNotFound
+      @vote = Vote.find_by_openstates_id(params[:id], :include => [:bill])
+    end
+
     @vote || resource_not_found
   end  
 end
