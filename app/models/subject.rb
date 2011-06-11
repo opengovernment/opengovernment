a7535be4deeba01147736cee0fab8c5bafd181e6
tashian <carl@ppolitics.org>
2011-06-11 11:08:24 -0700

    Hot subjects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -7,8 +7,20 @@ class Subject < ActiveRecord::Base
 
   scope :with_bill_count, { :select => 'subjects.*, count(bills.id) as bill_count', :joins => :bills, :group => 'subjects.id, subjects.name, subjects.code, subjects.created_at, subjects.updated_at' }
 
-  def self.with_session_bill_count(session_ids)
-    find_by_sql(['select s.*, coalesce(bsa.bill_count, 0) as bill_count from subjects s left outer join (select subject_id, count(bill_id) as bill_count from bills_subjects bs join bills b on (bs.bill_id = b.id) where b.session_id in (?) group by subject_id) bsa on (s.id = bsa.subject_id) order by s.name', session_ids])
+  def self.with_session_bill_count(session_ids, ops = {})
+    ops[:limit] ||= 1000
+    ops[:order] ||= :name
+
+    order_sql = case ops[:order]
+    when :popularity
+      'bill_count desc'
+    when :name
+      's.name'
+    else
+      's.name'
+    end
+  
+    find_by_sql(["select s.*, coalesce(bsa.bill_count, 0) as bill_count from subjects s left outer join (select subject_id, count(bill_id) as bill_count from bills_subjects bs join bills b on (bs.bill_id = b.id) where b.session_id in (?) group by subject_id) bsa on (s.id = bsa.subject_id) order by #{order_sql} limit ?", session_ids, ops[:limit]])
   end
 
   acts_as_taggable
