fd5ad73b7faafcebbadb71c11a037c9e1be5cf71
tashian <carl@ppolitics.org>
2011-06-10 17:05:05 -0700

    Rails 3.0.8; turn off syslog logger; fix #flash message container styling

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,6 @@ OpenGov::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server
