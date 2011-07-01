68f07c8071b8bbd48df133ca73385c6e3412e40d
tashian <carl@ppolitics.org>
2011-07-01 15:59:09 -0500

    Most viewed people, with pics

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -225,6 +225,7 @@ OG = window.OG || {};
     OG.Widget.prototype = function () {
       var http = isHttps ? 'https://' : 'http://';
       var domain = 'opengovernment.org';
+      domain = '127localhost.com:3000'
 
       // From http://javascriptweblog.wordpress.com/2010/11/29/json-and-jsonp/
       var jsonp = {
