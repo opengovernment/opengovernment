106b9a0f06bdcac7f6f15c701558735f67ef8972
James McKinney <james@slashpoundbang.com>
2012-11-20 16:18:34 -0500

    get search_spec.rb passing

--- spec/integration/search_spec.rb
+++ spec/integration/search_spec.rb
@@ -1,6 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Search" do
+  fixtures :states
+
   before do
     @texas = states(:tx)
     @request.host = "#{@texas.abbrev}.example.org"
@@ -9,17 +11,16 @@ describe "Search" do
 
   it "should have the search box" do
     within("header") do
-      within("div.search > form") do
+      within("div.search_form > form") do
         page.should have_css("select")
         page.should have_css("input#q")
-        page.should have_css("input#")
       end
     end
   end
 
   context "by everything" do
     it "should show all the search results" do
-      within("header > div.search > form") do
+      within("header > div.search_form > form") do
 
       end
     end
@@ -27,7 +28,7 @@ describe "Search" do
 
   context "by bills" do
     it "should show matching bills" do
-      within("header > div.search > form") do
+      within("header > div.search_form > form") do
 
       end
     end
@@ -35,7 +36,7 @@ describe "Search" do
 
   context "by legislators" do
     it "should show matching legislators" do
-      within("header > div.search > form") do
+      within("header > div.search_form > form") do
 
       end
     end
@@ -43,7 +44,7 @@ describe "Search" do
 
   context "by committes" do
     it "should show matching committees" do
-      within("header > div.search > form") do
+      within("header > div.search_form > form") do
 
       end
     end
@@ -51,7 +52,7 @@ describe "Search" do
 
   context "by contributions" do
     it "should show matching contributions" do
-      within("header > div.search > form") do
+      within("header > div.search_form > form") do
 
       end
     end
